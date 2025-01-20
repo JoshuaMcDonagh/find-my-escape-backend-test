7f2c8a91ffa4325226e0d746b850041d2221c73b
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-20 12:34:39 +0000

    feat:within the User Class i removed the @NoArgsConstructor and @allArgsConstructor and instead added all a no args contructer and a constructer aswell as all its getters and setters
    the reson i removed both annotations is because i have found that the @NoArgsConstructor and @AllArgsConstructor usually do not work half the time and it is safer to just not use it hence why i removed them

--- src/main/java/com/tamworth/find_my_escape_backend/model/User.java
+++ src/main/java/com/tamworth/find_my_escape_backend/model/User.java
@@ -8,8 +8,6 @@ import java.util.Set;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @Table(name = "USER")
 public class User {
@@ -48,6 +46,54 @@ public class User {
 //            uniqueConstraints = @UniqueConstraint(columnNames = {"userId", "activityId"})
 //    )
 
+    public User() {
+    }
 
+    public User(@NonNull String userId, String name, String emailAddress, String current_Search, Set<FavouriteLocation> favouriteLocations) {
+        this.userId = userId;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.current_Search = current_Search;
+        this.favouriteLocations = favouriteLocations;
+    }
 
+    public @NonNull String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(@NonNull String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getCurrent_Search() {
+        return current_Search;
+    }
+
+    public void setCurrent_Search(String current_Search) {
+        this.current_Search = current_Search;
+    }
+
+    public Set<FavouriteLocation> getFavouriteLocations() {
+        return favouriteLocations;
+    }
+
+    public void setFavouriteLocations(Set<FavouriteLocation> favouriteLocations) {
+        this.favouriteLocations = favouriteLocations;
+    }
 }
