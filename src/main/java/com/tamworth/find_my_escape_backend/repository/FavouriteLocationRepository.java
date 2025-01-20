1e9d49e9aa51a8ce1e37d3f0f5d7ef35914e423e
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-20 00:37:03 +0000

    feat: add custom query for favourite location delete

--- src/main/java/com/tamworth/find_my_escape_backend/repository/FavouriteLocationRepository.java
+++ src/main/java/com/tamworth/find_my_escape_backend/repository/FavouriteLocationRepository.java
@@ -1,9 +1,17 @@
 package com.tamworth.find_my_escape_backend.repository;
 
 import com.tamworth.find_my_escape_backend.model.FavouriteLocation;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface FavouriteLocationRepository extends CrudRepository<FavouriteLocation, Long> {
+public interface FavouriteLocationRepository extends JpaRepository<FavouriteLocation, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM FavouriteLocation fl WHERE fl.locationId = :locationId AND fl.favLocationUser.userId = :userId")
+    void deleteByLocationIdAndUserId(Long locationId, String userId);
 }
