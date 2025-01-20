1ff68c7ea79bca562d9d37208803feca6b020814
zohair-1995 <zohair.abidi1995@outlook.com>
2025-01-20 13:07:26 +0000

    added the implemetation of the findUserById method within the UserServiceImpl

--- src/main/java/com/tamworth/find_my_escape_backend/service/UserServiceImpl.java
+++ src/main/java/com/tamworth/find_my_escape_backend/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.tamworth.find_my_escape_backend.service;
 
+import com.tamworth.find_my_escape_backend.exception.ResourceNotFoundException;
 import com.tamworth.find_my_escape_backend.model.User;
 import com.tamworth.find_my_escape_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -13,7 +16,12 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User findUserById(String userId) {
-        return null;
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new ResourceNotFoundException("For the specified user id: " + userId + ", there is no user information that is found within the database!");
+        } else {
+            return user.get();
+        }
     }
 
     @Override
