bbbaffd5f3ee216f8533429563ac8482d05f3671
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-21 10:57:21 +0000

    feat: update FavouriteActivity service to get a list of activities for a userid and location id

--- src/main/java/com/tamworth/find_my_escape_backend/service/FavouriteActivityService.java
+++ src/main/java/com/tamworth/find_my_escape_backend/service/FavouriteActivityService.java
@@ -2,7 +2,10 @@ package com.tamworth.find_my_escape_backend.service;
 
 import com.tamworth.find_my_escape_backend.model.FavouriteActivity;
 
+import java.util.List;
+
 public interface FavouriteActivityService {
-    FavouriteActivity addActivityToLocation(FavouriteActivity activity, Long locationId);
-    void removeActivityFromLocation(Long activityId);
+    List<FavouriteActivity> getFavouriteActivities(String userId, Long locationId);
+    FavouriteActivity addFavouriteActivity(String userId, Long locationId, FavouriteActivity activity);
+    void removeFavouriteActivity(String userId, Long locationId, Long activityId);
 }
