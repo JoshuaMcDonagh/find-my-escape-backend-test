520c7761140964c535bf03ce74199e4612668187
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-21 13:36:16 +0000

    feat: update locations repository

--- src/main/java/com/tamworth/find_my_escape_backend/repository/LocationsRepository.java
+++ src/main/java/com/tamworth/find_my_escape_backend/repository/LocationsRepository.java
@@ -4,6 +4,9 @@ import com.tamworth.find_my_escape_backend.model.Locations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocationsRepository extends JpaRepository<Locations, Long> {
+    Optional<Locations> findByLocationName(String locationName);
 }
