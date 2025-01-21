6707aaafbbb34d85947f3c281b3e97e63d86b50a
Mohamed Fraitis <mfraitis1@gmail.com>
2025-01-21 00:11:24 +0000

    feat: update FavouriteActivityRepository

--- src/main/java/com/tamworth/find_my_escape_backend/repository/FavouriteActivityRepository.java
+++ src/main/java/com/tamworth/find_my_escape_backend/repository/FavouriteActivityRepository.java
@@ -1,9 +1,9 @@
 package com.tamworth.find_my_escape_backend.repository;
 
 import com.tamworth.find_my_escape_backend.model.FavouriteActivity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FavouriteActivityRepository extends CrudRepository<FavouriteActivity, String> {
+public interface FavouriteActivityRepository extends JpaRepository<FavouriteActivity, Long> {
 }
