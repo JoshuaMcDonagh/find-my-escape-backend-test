63d9f0464a276606544a5f61b4a807b65e494637
Joshua McDonagh <joshuamichaelmcdonagh@gmail.com>
2025-01-21 09:59:46 +0000

    refactor: remove redundant class imports in location service and controller

--- src/main/java/com/tamworth/find_my_escape_backend/service/LocationInformationService.java
+++ src/main/java/com/tamworth/find_my_escape_backend/service/LocationInformationService.java
@@ -1,9 +1,6 @@
 package com.tamworth.find_my_escape_backend.service;
 
-import com.tamworth.find_my_escape_backend.model.Activity;
 import com.tamworth.find_my_escape_backend.model.Location;
-
-import java.util.List;
 
 public interface LocationInformationService {
     Location findLocationInformation(String locationName);
